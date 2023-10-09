96a2909ecc8bf06609a0b22d889cfbb2f8d2a946
Thiago Fernando <thiago.fernando@msn.com>
2023-10-09 09:56:01 -0300

    criado campo de check

--- src/pages/propostas/cadastros/form/steps/formulario-certificacao.tsx
+++ src/pages/propostas/cadastros/form/steps/formulario-certificacao.tsx
@@ -1,11 +1,39 @@
-import { FormInstance } from 'antd';
-import React from 'react';
+import { Col, Form, FormInstance, Radio, RadioChangeEvent, Row } from 'antd';
+import React, { useState } from 'react';
 type FormDatasProps = {
   form: FormInstance;
 };
 
 const FormularioCertificacao: React.FC<FormDatasProps> = () => {
-  return 'Seção em desenvolvimento!';
+  const [valuePossuiCertificado, setValuePossuiCertificado] = useState(false);
+  const obterPossuiCertificado = (e: RadioChangeEvent) => {
+    console.log('radio checked', e.target.value);
+    setValuePossuiCertificado(e.target.value);
+  };
+  return (
+    <>
+      <Col>
+        <Row>
+          <Col>
+            <Form.Item
+              label='Possui Certificado'
+              name='criterioCertificacao'
+              rules={[{ required: true }]}
+            >
+              <Radio.Group
+                onChange={obterPossuiCertificado}
+                value={valuePossuiCertificado}
+                defaultValue={false}
+              >
+                <Radio value={true}>Sim</Radio>
+                <Radio value={false}>Não</Radio>
+              </Radio.Group>
+            </Form.Item>
+          </Col>
+        </Row>
+      </Col>
+    </>
+  );
 };
 
 export default FormularioCertificacao;
