caaa4e3a2b56403770b557bc2fa1b5989511ac86
Thiago Fernando <thiago.fernando@msn.com>
2023-10-30 16:42:12 -0300

    Exibir alerta ao fechar modal

--- src/pages/propostas/cadastros/form/steps/formulario-profissionais/components/lista-regentes/drawer/index.tsx
+++ src/pages/propostas/cadastros/form/steps/formulario-profissionais/components/lista-regentes/drawer/index.tsx
@@ -36,10 +36,23 @@ const DrawerRegente: React.FC<DrawerRegenteProps> = ({ openModal, onCloseModal, 
   const propostaId = paramsRoute?.id || 0;
 
   const fecharModal = (reloadData = false) => {
-    onCloseModal();
-    formDrawer.resetFields();
-    if (reloadData) {
-      tableState.reloadData();
+    if (formDrawer.isFieldsTouched()) {
+      confirmacao({
+        content: DESEJA_CANCELAR_ALTERACOES,
+        onOk() {
+          onCloseModal();
+          formDrawer.resetFields();
+          if (reloadData) {
+            tableState.reloadData();
+          }
+        },
+      });
+    } else {
+      onCloseModal();
+      formDrawer.resetFields();
+      if (reloadData) {
+        tableState.reloadData();
+      }
     }
   };
 
