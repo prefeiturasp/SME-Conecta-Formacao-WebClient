604f2b9793af0dc5fa1d078bc01c87e2bd6862de
Neder Hayden <nedersimoes@gmail.com>
2024-01-16 09:36:15 -0400

    111861 - ajuste de mensagem do botao voltar e adicionado paddingtop nos botoes de acao

--- src/core/constants/mensagens.ts
+++ src/core/constants/mensagens.ts
@@ -8,6 +8,7 @@ export const ERRO_INFORMAR_LOGIN_RECUPERACAO_SENHA =
   'Você precisa um usuário para recuperação de senha';
 export const ERRO_RECUPERACAO_SENHA = 'Erro ao tentar recuperar senha';
 export const DESEJA_EXCLUIR_REGISTRO = 'Você tem certeza que deseja excluir este registro?';
+export const SUA_INSCRICAO_NAO_FOI_ENVIADA = 'Sua inscrição não foi enviada. Deseja enviar agora?';
 export const DESEJA_SALVAR_ALTERACOES_AO_SAIR_DA_PAGINA =
   'Suas alterações não foram salvas, deseja salvar agora?';
 export const REGISTRO_EXCLUIDO_SUCESSO = 'Registro excluído com sucesso!';
