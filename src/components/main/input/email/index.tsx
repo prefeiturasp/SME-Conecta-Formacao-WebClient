a55e56cac65a227bafebac88d8b74ce6bdc48347
Thiago Fernando <thiago.fernando@msn.com>
2024-02-28 13:58:09 -0300

    remover espaço do email

--- src/components/main/input/email/index.tsx
+++ src/components/main/input/email/index.tsx
@@ -1,5 +1,5 @@
 import { Form, FormItemProps, Input, InputProps } from 'antd';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 type InputEmailProps = {
   inputProps?: InputProps;
@@ -7,19 +7,45 @@ type InputEmailProps = {
 };
 
 const InputEmail: React.FC<InputEmailProps> = ({ inputProps, formItemProps }) => {
+  const form = Form.useFormInstance();
+
+  const [exibirErro, setExibirErro] = useState(true);
+
+  const removerEspacoEmail = () => {
+    const email = form.getFieldValue('email');
+    const emailSemEspaco = email.trim();
+    form.setFieldValue('email', emailSemEspaco);
+    setExibirErro(false);
+    form.setFields([
+      {
+        name: 'email',
+        errors: [],
+      },
+    ]);
+    if (!emailSemEspaco.includes('@')) {
+      setExibirErro(true);
+    }
+  };
+
+  useEffect(() => {
+    setExibirErro(!!formItemProps?.required);
+  }, [formItemProps?.required]);
   return (
     <Form.Item
       label='E-mail'
       name='email'
       rules={[
         { required: !!formItemProps?.required },
-        { type: 'email', message: 'Não é um e-mail válido' },
+        { type: 'email', message: exibirErro ? 'Não é um e-mail válido' : '' },
       ]}
       {...formItemProps}
     >
       <Input
         placeholder='Informe o e-mail'
         autoComplete='off'
+        onChange={(_) => {
+          removerEspacoEmail();
+        }}
         maxLength={100}
         id='INPUT_EMAIL'
         {...inputProps}
