ab08ecfef246aa22df629affc205992220d8bb5d
Juliano Martins <juliano.campos@amcom.com.br>
2024-05-30 16:03:57 -0400

    Task 120010: FRONT - Critério 1 - Botão "Realizar sorteio" - Endpoint

--- src/pages/formacoes/inscricoes/listagem/index.tsx
+++ src/pages/formacoes/inscricoes/listagem/index.tsx
@@ -5,6 +5,12 @@ import { useNavigate } from 'react-router-dom';
 import DataTable from '~/components/lib/card-table';
 import { ROUTES } from '~/core/enum/routes-enum';
 import { FiltroInscricoesProps } from '..';
+import { Button } from 'antd';
+import { CF_BUTTON_SORTEAR } from '~/core/constants/ids/button/intex';
+import { INSCRICAO_CONFIRMA_SORTEIO } from '~/core/constants/mensagens';
+import { confirmacao } from '~/core/services/alerta-service';
+import { sortearInscricao } from '~/core/services/inscricao-service';
+import { notification } from '~/components/lib/notification';
 
 interface InscricoesListaPaginadaProps {
   filters?: FiltroInscricoesProps;
@@ -19,10 +25,22 @@ interface InscricoesProps {
 }
 
 interface TurmasProps {
+  propostaTurmaId: number;
   nomeTurma: string;
   data: string;
   quantidadeVagas: number;
   quantidadeInscricoes: number;
+  quantidadeConfirmada: number;
+  quantidadeAguardandoAnalise: number;
+  quantidadeEmEspera: number;
+  quantidadeCancelada: number;
+  quantidadeDisponivel: number;
+  quantidadeExcedida: number;
+  permissao: TurmasPermissaoProps;
+}
+
+interface TurmasPermissaoProps {
+  podeRealizarSorteio: boolean;
 }
 
 export const InscricoesListaPaginada: React.FC<InscricoesListaPaginadaProps> = ({
@@ -39,12 +57,52 @@ export const InscricoesListaPaginada: React.FC<InscricoesListaPaginadaProps> = (
   const onClickEditar = (row: InscricoesProps) =>
     navigate(`${ROUTES.FORMACAOES_INSCRICOES}/editar/${row.id}`, { replace: true, state: row });
 
+  const onClickSortear = (propostaTurmaId: number) => {
+    confirmacao({
+      content: INSCRICAO_CONFIRMA_SORTEIO,
+      onOk() {
+        sortearInscricao(propostaTurmaId).then((response) => {
+          if (response.sucesso) {
+            notification.success({
+              message: response?.dados?.mensagem,
+              description: 'Registro excluído com sucesso',
+            });
+          }
+        });
+      },
+    });
+  };
+
   const expandedRowRender = (record: InscricoesProps) => {
     const columns: ColumnsType<TurmasProps> = [
+      { title: 'id', dataIndex: 'propostaTurmaId' },
       { title: 'Turma', dataIndex: 'nomeTurma' },
       { title: 'Data', dataIndex: 'data' },
-      { title: 'Quantidade de vagas', dataIndex: 'quantidadeVagas' },
-      { title: 'Quantidade de inscrições', dataIndex: 'quantidadeInscricoes' },
+      { title: 'Vagas', dataIndex: 'quantidadeVagas' },
+      { title: 'Inscrições', dataIndex: 'quantidadeInscricoes' },
+      { title: 'Aguardando análise', dataIndex: 'quantidadeAguardandoAnalise' },
+      { title: 'Em espera', dataIndex: 'quantidadeEmEspera' },
+      { title: 'Confirmadas', dataIndex: 'quantidadeConfirmada' },
+      { title: 'Canceladas', dataIndex: 'quantidadeCancelada' },
+      { title: 'Vagas disponíveis', dataIndex: 'quantidadeDisponivel' },
+      { title: 'Vagas excedidas', dataIndex: 'quantidadeExcedida' },
+      {
+        title: 'Ações',
+        align: 'center',
+        width: '165px',
+        render: (_, record) => (
+          <>
+            <Button
+              block
+              onClick={() => onClickSortear(record.propostaTurmaId)}
+              id={CF_BUTTON_SORTEAR}
+              disabled={!record.permissao.podeRealizarSorteio}
+            >
+              Sortear inscrições
+            </Button>
+          </>
+        ),
+      },
     ];
 
     return (
