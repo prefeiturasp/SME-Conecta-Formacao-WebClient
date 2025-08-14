333b88e9aa103f1d9033e695017f518b9fff96ba
Ewerton Richieri Lopes <ewerton.lopes@spassu.com.br>
2025-08-14 12:00:15 -0300

    Ambiente de teste configurado

---
+++ jest.config.js
@@ -0,0 +1,63 @@
+export default {
+  collectCoverage: false,
+  coverageReporters: ['text', 'lcov', 'json', 'html'],
+  coverageDirectory: 'coverage',
+  preset: 'ts-jest/presets/default-esm',
+  extensionsToTreatAsEsm: ['.ts', '.tsx'],
+  transform: {
+    '^.+\\.(ts|tsx)$': ['ts-jest', {
+      useESM: true,
+    }],
+    '^.+\\.(js|jsx)$': 'babel-jest',
+    '^.+\\.(svg|jpg|jpeg|png|woff|woff2|eot|ttf|otf)$': 'jest-transform-stub',
+  },
+  testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    '^~/(.*)$': '<rootDir>/src/$1',
+    '\\.(svg|jpg|jpeg|png|woff|woff2|eot|ttf|otf)$': '<rootDir>/__mocks__/fileMock.js',
+    '\\.(css|less|scss|css\\.js)$': 'identity-obj-proxy',
+    '\\.(css|less|scss)$': 'identity-obj-proxy',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  collectCoverageFrom: [
+    'src/@legacy/**/*.{js,jsx,ts,tsx}',
+    'src/pages/**/*.{js,jsx,ts,tsx}',
+    'src/components/**/*.{js,jsx,ts,tsx}',
+    '!src/**/*.test.{js,jsx,ts,tsx}',
+    '!src/**/__mocks__/**',
+    '!src/**/*.css.js',
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 1,
+      functions: 1,
+      lines: 1,
+      statements: 1,
+    },
+  },
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
+  transformIgnorePatterns: [
+    '/node_modules/(?!(antd|@ant-design|rc-.+|@babel/runtime)/).*/',
+  ],
+  globals: {
+    'ts-jest': {
+      useESM: true,
+    },
+  },
+  extensionsToTreatAsEsm: ['.ts', '.tsx', '.jsx'],
+  coverageProvider: 'v8',
+  coveragePathIgnorePatterns: [
+    '/node_modules/',
+    '/build/',
+    '/dist/',
+    '/__mocks__/',
+    '/.*.d.ts$/',
+    '/.*.config.js$/',
+    '/.*.test.js$/',
+    '/.*.css.js$/',
+    '/src/index\\.tsx$',
+    '/src/@types',
+    '/src/app\\.tsx$',
+    '/.*\\/style\\.js$',
+  ],
+};
