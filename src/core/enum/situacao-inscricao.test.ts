b5c4cdafe86e282f27e3f42847deb27b6fa119a9
Anderson Oliveira Magalhães <andoliveira4@gmail.com>
2025-12-26 09:53:29 -0300

    ajuste teste unitários

--- src/core/enum/situacao-inscricao.test.ts
+++ src/core/enum/situacao-inscricao.test.ts
@@ -7,19 +7,21 @@ describe('SituacaoInscricao Enum', () => {
     expect(SituacaoInscricao.EmAnalise).toBe(3);
     expect(SituacaoInscricao.Cancelada).toBe(4);
     expect(SituacaoInscricao.EmEspera).toBe(5);
+    expect(SituacaoInscricao.Transferida).toBe(6);
   });
 
   test('deve ter todas as labels corretas no SituacaoInscricaoTagDisplay', () => {
     expect(SituacaoInscricaoTagDisplay[SituacaoInscricao.Confirmada]).toBe('Confirmada');
     expect(SituacaoInscricaoTagDisplay[SituacaoInscricao.Enviada]).toBe('Enviada');
-    expect(SituacaoInscricaoTagDisplay[SituacaoInscricao.EmAnalise]).toBe('Em Análise');
+    expect(SituacaoInscricaoTagDisplay[SituacaoInscricao.EmAnalise]).toBe('Aguardando Análise');
     expect(SituacaoInscricaoTagDisplay[SituacaoInscricao.Cancelada]).toBe('Cancelada');
     expect(SituacaoInscricaoTagDisplay[SituacaoInscricao.EmEspera]).toBe('Em Espera');
+    expect(SituacaoInscricaoTagDisplay[SituacaoInscricao.Transferida]).toBe('Transferida');
   });
 
-  test('deve ter exatamente 5 situações', () => {
+  test('deve ter exatamente 6 situações', () => {
     const situacoes = Object.keys(SituacaoInscricao).filter((key) => !Number.isNaN(Number(key)));
-    expect(situacoes.length).toBe(5);
+    expect(situacoes.length).toBe(6);
   });
 
   test('todas as situações devem ter uma label correspondente', () => {
