dca185d39199c23433ca68f0b2cd790b83497e56
Mônica Chiesa <monica.chiesa@universo.univates.br>
2026-06-30 09:00:31 -0300

    Fazer condicional para download do arquivo

--- src/pages/formacoes/lista-presenca-codaf/index.tsx
+++ src/pages/formacoes/lista-presenca-codaf/index.tsx
@@ -250,18 +250,25 @@ const ListaPresencaCodaf: React.FC = () => {
     const hasCodigoCursoEol = record.codigoCursoEol != null;
     const isAguardandoDF = record.status === 2;
     const isFinalizado = record.status === 4;
+    const isIniciado = record.status === 1;
     const isCertificacaoConcluida = record.statusCertificacaoTurma === 4;
     const podeGerarComoComum = isAguardandoDF && hasCodigoCursoEol;
     const podeGerarComoAdmin = isFinalizado && ehPerfilAdminDf;
-    const podeGerarTxtEol = podeGerarComoComum || podeGerarComoAdmin;
+    const podeGerarTxtEol = podeGerarComoComum || podeGerarComoAdmin || hasCodigoCursoEol;
 
     const getTooltipMessage = () => {
       if (podeGerarTxtEol) {
         return 'Clique para gerar TXT EOL';
       }
+      
       if (isAguardandoDF && !hasCodigoCursoEol) {
         return 'Informe o valor de Cód. curso EOL para gerar o arquivo.';
       }
+
+      if (isIniciado && !hasCodigoCursoEol) {
+        return 'Informe o valor de Cód. curso EOL para gerar o arquivo.';
+      }
+
       return 'Função ativa apenas para a situação Aguardando DF com valor de Cod. Curso EOL informado';
        
     };
